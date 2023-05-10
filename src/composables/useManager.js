bbd6ffe0b14a179811e53968a3e254c3342adbd5
chad <1411430556@qq.com>
2023-05-11 02:42:07 +0800

    组合式API封装简化代码

---
+++ src/composables/useManager.js
@@ -0,0 +1,84 @@
+import { reactive, ref } from 'vue'
+import { logOut, updatePassword } from '~/api/manager.js'
+import { showModal, toast } from '~/composables/util.js'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+
+export function useRepassword () {
+  const router = useRouter()
+  const store = useStore()
+  // 修改密码-抽屉弹出
+  const formDrawerRef = ref(null)
+
+  const form = reactive({
+    oldpassword: '',
+    password: '',
+    repassword: '',
+  })
+
+//表单验证规则
+  const rules = reactive({
+    oldpassword: [
+      { required: true, message: '旧密码不能为空', trigger: 'blur' },
+    ],
+    password: [
+      { required: true, message: '新密码不能为空', trigger: 'blur' },
+    ],
+    repassword: [
+      { required: true, message: '确认密码不能为空', trigger: 'blur' },
+    ],
+  })
+
+  const formRef = ref(null)
+
+  const onSubmit = () => {
+    formRef.value.validate((valid) => {
+      if (!valid) {
+        return false
+      }
+      // loading 状态打开
+      formDrawerRef.value.showLoading()
+      updatePassword(form).then(() => {
+        toast('修改密码成功，请重新登录', 'success')
+        store.dispatch('logout')
+        router.push('/login')
+      }).finally(() => {
+        // loading 状态关闭
+        formDrawerRef.value.hideLoading()
+      })
+    })
+  }
+
+  const openRePasswordForm = () => formDrawerRef.value.open()
+
+  return {
+    formDrawerRef,
+    form,
+    rules,
+    formRef,
+    onSubmit,
+    openRePasswordForm,
+  }
+}
+
+export function useLogout () {
+  const router = useRouter()
+  const store = useStore()
+  // 退出登录的逻辑
+  function handleLogout () {
+    showModal('是否退出登录？').then(res => {
+      logOut().finally(() => {
+        // 调用store状态管理里的logout函数移除 cookie 里的token,清除当前用户状态 vuex
+        store.dispatch('logout')
+        //跳转回登录页
+        router.push('/login')
+        // 提示退出登录成功
+        toast('已退出登录', 'success')
+      })
+    })
+  }
+
+  return {
+    handleLogout,
+  }
+}
--- src/layouts/components/FHeader.vue
+++ src/layouts/components/FHeader.vue
@@ -1,66 +1,32 @@
 <script setup>
-import { showModal, toast } from '~/composables/util.js'
-import { logOut, updatePassword } from '~/api/manager.js'
-import { useRouter } from 'vue-router'
-import { useStore } from 'vuex'
 import { useFullscreen } from '@vueuse/core'
-import { ref, reactive } from 'vue'
 import FormDrawer from '~/components/FormDrawer.vue'
+import { useRepassword, useLogout } from '~/composables/useManager.js'
+
+/*
+* 这里面很多逻辑都在 ~/composables/useManager.js 里面
+*  */
 
 // isFullscreen 是否全屏状态  toggle 切换全屏与退出全屏
 const { isFullscreen, toggle } = useFullscreen()
+// 解构出来
+const {
+  formDrawerRef,
+  form,
+  rules,
+  formRef,
+  onSubmit,
+  openRePasswordForm,
+} = useRepassword()
 
-const router = useRouter()
-const store = useStore()
-
-// 修改密码-抽屉弹出
-const formDrawerRef = ref(null)
-
-const form = reactive({
-  oldpassword: '',
-  password: '',
-  repassword: '',
-})
-
-//表单验证规则
-const rules = reactive({
-  oldpassword: [
-    { required: true, message: '旧密码不能为空', trigger: 'blur' },
-  ],
-  password: [
-    { required: true, message: '新密码不能为空', trigger: 'blur' },
-  ],
-  repassword: [
-    { required: true, message: '确认密码不能为空', trigger: 'blur' },
-  ],
-})
-
-const formRef = ref(null)
-
-const onSubmit = () => {
-  formRef.value.validate((valid) => {
-    if (!valid) {
-      return false
-    }
-    // loading 状态打开
-    formDrawerRef.value.showLoading()
-    updatePassword(form).then(() => {
-      toast('修改密码成功，请重新登录', 'success')
-      store.dispatch('logout')
-      router.push('/login')
-    }).finally(() => {
-      // loading 状态关闭
-      formDrawerRef.value.hideLoading()
-    })
-  })
-}
+// 退出登录
+const { handleLogout } = useLogout()
 
 // 控制修改密码还是退出登录
 const handleCommand = (c) => {
   switch (c) {
     case 'rePassword':
-      // showDrawer.value = true
-      formDrawerRef.value.open()
+      openRePasswordForm()
       break
     case 'logout':
       handleLogout()
@@ -70,20 +36,6 @@ const handleCommand = (c) => {
 
 // 刷新逻辑
 const handleRefresh = () => location.reload()
-
-// 退出登录的逻辑
-function handleLogout () {
-  showModal('是否退出登录？').then(res => {
-    logOut().finally(() => {
-      // 调用store状态管理里的logout函数移除 cookie 里的token,清除当前用户状态 vuex
-      store.dispatch('logout')
-      //跳转回登录页
-      router.push('/login')
-      // 提示退出登录成功
-      toast('已退出登录', 'success')
-    })
-  })
-}
 </script>
 
 <template>
