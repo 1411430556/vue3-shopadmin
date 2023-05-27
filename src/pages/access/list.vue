55dfeafb5af1091eb85e9d056a1cb86fc8be205a
chad <1411430556@qq.com>
2023-05-27 18:44:56 +0800

    树形控件自定义节点渲染

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -3,6 +3,7 @@ import ListHeader from '~/components/ListHeader.vue'
 import { getRuleList } from '~/api/rule.js'
 import { useInitTable } from '~/composables/useCommon.js'
 import { ref } from 'vue'
+import { Comment } from '@element-plus/icons-vue'
 
 const defaultExpandedKeys = ref([])
 
@@ -15,7 +16,7 @@ const {
   onGetListSuccess: (value) => {
     tableData.value = value.list
     defaultExpandedKeys.value = value.list.map(item => item.id)
-  }
+  },
 })
 </script>
 
@@ -23,10 +24,38 @@ const {
   <el-card shadow="never" class="border-0">
     <ListHeader @refresh="getData"/>
     <el-tree :data="tableData" :props="{label: 'name', children: 'child'}"
-             v-loading="loading" node-key="id" :default-expanded-keys="defaultExpandedKeys"/>
+             v-loading="loading" node-key="id" :default-expanded-keys="defaultExpandedKeys">
+      <template #default="{ node, data }">
+        <!--左侧-->
+        <div class="custom-tree-node">
+          <el-tag size="small" :type="data.menu ? '' : 'info'">{{ data.menu ? '菜单' : '权限' }}</el-tag>
+          <el-icon v-if="data.icon" :size="16" class="ml-2">
+            <component :is="data.icon"/>
+          </el-icon>
+          <span>{{ data.name }}</span>
+          <!--右侧-->
+          <div class="ml-auto">
+            <el-switch :model-value="data.status" :active-value="1" :inactive-value="0"/>
+            <el-button text type="primary" size="small">修改</el-button>
+            <el-button text type="primary" size="small">增加</el-button>
+            <el-button text type="primary" size="small">删除</el-button>
+          </div>
+        </div>
+      </template>
+    </el-tree>
   </el-card>
 </template>
 
 <style scoped>
+.custom-tree-node {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  font-size: 14px;
+  padding-right: 8px;
+}
 
+/deep/ .el-tree-node__content {
+  padding: 20px 0;
+}
 </style>
