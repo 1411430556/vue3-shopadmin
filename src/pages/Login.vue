0ba4034840a379199f95e6732a4961ad2bce4dd8
chad <1411430556@qq.com>
2024-04-30 10:37:28 +0800

    更新登录页背景动态图地址为腾讯云OSS

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -70,7 +70,7 @@ const onSubmit = () => {
   <el-row class="login-container">
     <el-col :lg="16" :md="12" class="left">
       <div>
-        <img src="http://43.136.216.54/view.php/c323aed6e695604258029a662f241f24.gif" alt="">
+        <img src="https://image-1256031104.cos.ap-chengdu.myqcloud.com/JNPF.gif" alt="">
       </div>
     </el-col>
 
