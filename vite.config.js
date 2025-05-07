2d24e2fad792b5a1c16383ab3200ead0b9bdeaca
Faker <1411430556@qq.com>
2025-05-07 12:32:33 +0800

    🔧chore: 更新.gitignore和vite.config.js以支持环境变量
    在.gitignore中添加了对.env文件的忽略，以保护敏感信息。同时，修改vite.config.js以使用loadEnv函数加载环境变量，提升了配置的安全性和灵活性。

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import vitePluginsAutoI18n, { YoudaoTranslator } from 'vite-auto-i18n-plugin'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -10,42 +10,46 @@ import WindiCSS from 'vite-plugin-windicss'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      // 设置 src 别名
-      '~': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    // host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://ceshi13.dishait.cn',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  let root = process.cwd()
+  const env = loadEnv(mode, root, '')
+  return {
+    resolve: {
+      alias: {
+        // 设置 src 别名
+        '~': path.resolve(__dirname, 'src'),
       },
     },
-  },
-  plugins: [
-    vue(),
-    WindiCSS(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    CodeInspectorPlugin({
-      bundler: 'vite',
-      editor: 'trae',
-    }),
-    vitePluginsAutoI18n({
-      targetLangList: ['en', 'ko', 'ja', 'ru', 'fr', 'de', 'es', 'pt', 'it', 'th'],
-      translator: new YoudaoTranslator({
-        appId: process.env.VITE_YOUDAO_APP_ID,
-        appKey: process.env.VITE_YOUDAO_APP_KEY,
+    server: {
+      // host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: 'http://ceshi13.dishait.cn',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    plugins: [
+      vue(),
+      WindiCSS(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
       }),
-    }),
-  ],
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      CodeInspectorPlugin({
+        bundler: 'vite',
+        editor: 'trae',
+      }),
+      vitePluginsAutoI18n({
+        targetLangList: ['en', 'ko', 'ja', 'ru', 'fr', 'de', 'es', 'pt', 'it', 'th'],
+        translator: new YoudaoTranslator({
+          appId: env.VITE_YOUDAO_APP_ID,
+          appKey: env.VITE_YOUDAO_APP_KEY,
+        }),
+      }),
+    ],
+  }
 })
